0a7de2a84d1ac2015492c0ab476547eda6c2ea86
Abdelkarim-Douadjia <karimkoki262@gmail.com>
2024-04-19 23:25:50 +0100

    0x0A. Configuration management

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,10 @@
-#!/usr/bin/pup
-# Install an especific version of flask (2.1.0)
-package {'flask':
+# Installs a package flask using pip3
+package { ['python3-pip']:
+  ensure => installed,
+}
+
+package { 'Flask':
   ensure   => '2.1.0',
-  provider => 'pip3'
+  provider => 'pip3',
 }
 
